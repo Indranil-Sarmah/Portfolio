b38e9983efd28eacb97eb244dc0f85715c276c6b
Indranil-Sarmah <isharma055@gmail.com>
2024-09-11 03:44:07 +0530

    changes

--- portfolio-website-master/components/footer.tsx
+++ portfolio-website-master/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
         <div className="flex justify-center items-center" >
-          ------- Made with 💗 and  <DiJavascript1 style={{ color: '#F7DF1E', fontSize: '1rem' }} /> -------
+          ------- Made with 💛 and  <DiJavascript1 style={{ color: '#F7DF1E', fontSize: '1rem' }} /> -------
         </div>
         <div><span style={{fontWeight:"bold"}}>Indranil Sarmah</span></div>
 
